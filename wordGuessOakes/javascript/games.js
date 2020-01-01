864e264e5c03e28ef799c51fb8373d937e177600
Patrick Oakes <patrickoakes1972@gmail.com>
2019-12-31 17:07:39 -0800

    commit Java

---
+++ wordGuessOakes/javascript/games.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+var bands = ['replacements', 'soul asylum', 'husker du', 'prince', 'pixies','cure',
+'new order', 'sonic youth', 'minuteman'];
+
+var guessedLetters =[];
+
+var letterToGuess = null;
+
+var guessesLeft = 10;
+
+var wins = 0;
+
+var losses = 0;
+
+var updateBandGuess = function () {
+letterToGuess = bands[Math.floor(Math.random * bands.length)];
+};
+
+var updateGuessesLeft = function () {
+    document.querySelector('#guesses-left').innerHTML = guessesLeft;
+   };
+
+   var updateGuessesSoFar = function () {
+       document.querySelector('#guesses-made').innerHTML = guessedLetters.join(", ");
+   };
+
