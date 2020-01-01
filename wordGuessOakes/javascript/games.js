909da3ec388df00bc756954ade97d1327995d04b
Patrick Oakes <patrickoakes1972@gmail.com>
2019-12-31 21:05:46 -0800

    full

--- wordGuessOakes/javascript/games.js
+++ wordGuessOakes/javascript/games.js
@@ -1,9 +1,5 @@
 
-
-
-
-
-
+/////Variable Area
 var bands = ['replacements', 'soul asylum', 'husker du', 'prince', 'pixies','cure',
 'new order', 'sonic youth', 'minuteman'];
 
@@ -17,6 +13,7 @@ var wins = 0;
 
 var losses = 0;
 
+/// functions 
 var updateBandGuess = function () {
 letterToGuess = bands[Math.floor(Math.random * bands.length)];
 };
@@ -39,3 +36,21 @@ var updateGuessesLeft = function () {
 
 updateBandGuess();
 updateGuessesLeft();
+
+//Full Function///MAIN FUNCTION/////////////////////
+// This function will capture the keyboard clicks.
+document.onkeydown = function (event) {
+   
+   
+    // It's going to reduce the guesses by one
+    guessesLeft--;
+
+    // Lowercase the letter
+    var letter = event.key.toLowerCase();
+
+    // Then add the guess to the guessedLetters array
+    guessedLetters.push(letter);
+
+    // Then its going to run the update functions
+    updateGuessesLeft();
+    updateGuessesSoFar();
